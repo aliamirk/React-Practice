483650a4abdc54edb2abd5b94c95b54c9d5a22b6
aliamirk <ali.bussiness01@gmail.com>
2023-05-28 19:24:13 +0500

    Use Hooks

--- Project-2/src/App.js
+++ Project-2/src/App.js
@@ -1,20 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // this being used outside creates an infinite loop as the component re renders upon every state change as each time this code is executed the re evaluates so better to use useEffect hook
+
+  // if (storedUserloggedinInfo === '1') {
+  //   setIsLoggedIn(true);
+  // }
+
+  useEffect(() => {
+
+    const storedUserloggedinInfo = localStorage.getItem("isloggedIn");
+    console.log(storedUserloggedinInfo);
+
+    if (storedUserloggedinInfo === "1") {
+      setIsLoggedIn(true);
+    }
+
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isloggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem('isloggedIn')
   };
 
   return (
